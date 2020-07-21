3577fe9e26bf9b0ab08372744040ad3e1fe927d8
jhyu <yujiahuiyu@gmail.com>
2020-07-21 11:37:30 +0800

    week14 添加鼠标事件

--- week14/component/main.js
+++ week14/component/main.js
@@ -84,6 +84,58 @@ class Carousel {
 		}
 		setTimeout(nextPic, 3000);
 
+		// 鼠标移动
+		root.addEventListener("mousedown", (event) => {
+			let startX = event.clientX, 
+				startY = event.clientY;
+			let nextPosition = (position + 1) % this.data.length;
+			let lastPosition = (position - 1 + this.data.length) % this.data.length;
+			
+			let current = children[position];
+			let next = children[nextPosition];
+			let last = children[lastPosition];
+
+			current.style.transition = "ease 0s";
+			next.style.transition = "ease 0s";
+			last.style.transition = "ease 0s";
+
+			current.style.transform = `translateX(${ - 500 * position}px)`;
+			next.style.transform = `translateX(${500 - 500 * lastPosition}px)`;  
+			last.style.transform = `translateX(${- 500 - 500 * nextPosition}px)`;
+
+			let move = (event) => {
+				current.style.transform = `translateX(${ event.clientX - startX - 500 * position}px)`;
+				next.style.transform = `translateX(${ event.clientX - startX - 500 - 500 * nextPosition}px)`;
+				last.style.transform = `translateX(${ event.clientX - startX + 500 - 500 * lastPosition}px)`;
+			};
+
+			let up = (event) => {
+				let offset = 0;
+
+				if (event.clientX - startX > 250) {
+					offset = 1;
+				} else if (event.clientX - startX < -250) {
+					offset = -1;
+				}
+
+				// 打开 transition
+				current.style.transition = "";
+				next.style.transition = "";
+				last.style.transition = "";
+
+				current.style.transform = `translateX(${ offset * 500 - 500 * position}px)`;
+				next.style.transform = `translateX(${ offset * 500 - 500 - 500 * nextPosition}px)`;
+				last.style.transform = `translateX(${ offset * 500 + 500 - 500 * lastPosition}px)`;
+
+				position =  (position - offset + this.data.length) % this.data.length;
+				document.removeEventListener("mousemove", move);
+				document.removeEventListener("mouseup", up);
+			};
+
+			document.addEventListener("mousemove", move);
+			document.addEventListener("mouseup", up);
+		});
+
 
 		return 	root;
 	}
